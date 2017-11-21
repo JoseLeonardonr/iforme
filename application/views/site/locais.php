bdc9e94404f5b9eaed3b08a6619f1f410ea5cf4a
José Leonardo <Jos Leonardo@10.0.12.121>
2017-11-20 22:05:24 -0200

    Ajustes css

--- application/views/site/locais.php
+++ application/views/site/locais.php
@@ -32,7 +32,7 @@
     
     <style>
        #map {
-        height: 400px;
+        height: 500px;
         width: 100%;
        }
     </style>
@@ -65,13 +65,7 @@
             </div>
             <footer class="mdl-mini-footer">
                 <div class="mdl-mini-footer__left-section">
-                    <div class="mdl-logo">Simple portfolio website</div>
-                </div>
-                <div class="mdl-mini-footer__right-section">
-                    <ul class="mdl-mini-footer__link-list">
-                        <li><a href="#">Help</a></li>
-                        <li><a href="#">Privacy & Terms</a></li>
-                    </ul>
+                    <div class="mdl-logo">Desenvolvido por:<br>Aline, José Leonardo, Josias, Matheus, Maykon, Pedro Henrique</div>
                 </div>
             </footer>
         </main>
